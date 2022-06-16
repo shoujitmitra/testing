24b2f66332b36cb95307cd1a56e3aad453f374e2
shoujitmitra <49413327+shoujitmitra@users.noreply.github.com>
2022-06-16 18:04:42 -0400

    Add files via upload

---
+++ setup_RAN_env_USM2_USM.sh
@@ -0,0 +1,11 @@
+# Load file
+#!/usr/bin/env bash
+
+########################################################################################################################
+export hostname=nfv127
+export router_id=1.1.1.14
+export dcsg_type=CE
+export api_payload='input-source'
+export api_port=9091
+export api_url='/tip/v1/CreateNE/oss/ne/'
+export bgp_number_of_neighbors=0
